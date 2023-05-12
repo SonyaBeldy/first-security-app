29d85cba6fdde4b6f90da642f3cfe69d0ed4a87f
Sonya <sofya.beldy@mail.ru>
2023-05-12 17:49:19 +1000

    get Person information

--- src/main/java/ru/sonyabeldy/FirstSecurityApp/controller/HelloController.java
+++ src/main/java/ru/sonyabeldy/FirstSecurityApp/controller/HelloController.java
@@ -1,12 +1,26 @@
 package ru.sonyabeldy.FirstSecurityApp.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.sonyabeldy.FirstSecurityApp.security.PersonDetails;
+import ru.sonyabeldy.FirstSecurityApp.services.PersonDetailsService;
 
 @Controller
 public class HelloController {
     @RequestMapping("/hello")
     public String sayHello() {
+        return "hello";
+    }
+
+    @GetMapping("/showUserInfo")
+    public String showUserInfo() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        PersonDetails personDetails = (PersonDetails) authentication.getPrincipal();
+        System.out.println(personDetails.getPerson());
+
         return "hello";
     }
 }
