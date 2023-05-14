8099a652a239c37f02bfa7a533c38c7f68a683b1
Sonya <sofya.beldy@mail.ru>
2023-05-14 13:33:22 +1000

    custom authentication page

--- src/main/java/ru/sonyabeldy/FirstSecurityApp/config/SecurityConfig.java
+++ src/main/java/ru/sonyabeldy/FirstSecurityApp/config/SecurityConfig.java
@@ -4,9 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import ru.sonyabeldy.FirstSecurityApp.services.PersonDetailsService;
 
 @Configuration
@@ -21,6 +27,28 @@ public class SecurityConfig {
         this.personDetailsService = personDetailsService;
     }
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable() //отключаем защиту от межсайтовой подделке запросов, SC даст отправить запрос без csrf токкена
+                .authorizeHttpRequests().requestMatchers("/auth/login", "/error").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/auth/login")
+                .loginProcessingUrl("/process_login")
+                .defaultSuccessUrl("/hello", true)
+                .failureUrl("/auth/login?error")
+                .permitAll();
+
+        return http.build();
+    }
+
+//    @Bean
+//    public WebSecurityCustomizer webSecurityCustomizer() {
+//        return (web) -> web.ignoring().antMatchers("/ignore1", "/ignore2");
+//    }
+
+
+    //настраиваем аутентификацию
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(personDetailsService);
     }
