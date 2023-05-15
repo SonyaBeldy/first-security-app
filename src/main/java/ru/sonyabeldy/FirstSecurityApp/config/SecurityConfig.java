bb9938b53294c02707cc308d416a8524681fa3be
Sonya <sofya.beldy@mail.ru>
2023-05-15 13:38:33 +1000

    password encryption

--- src/main/java/ru/sonyabeldy/FirstSecurityApp/config/SecurityConfig.java
+++ src/main/java/ru/sonyabeldy/FirstSecurityApp/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package ru.sonyabeldy.FirstSecurityApp.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,6 +12,7 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -46,19 +49,29 @@ public class SecurityConfig {
     }
 
 //    @Bean
+//    public DaoAuthenticationProvider authProvider() {
+//        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+//        authProvider.setUserDetailsService(personDetailsService);
+//        authProvider.setPasswordEncoder(getPasswordEncoder());
+//        return authProvider;
+//    }
+//    @Bean
+//    public AuthenticationManager authManager(HttpSecurity http) throws Exception {
+//        return http.getSharedObject(AuthenticationManagerBuilder.class)
+//                .authenticationProvider(authProvider())
+//                .build();
+//    }
+
+
+
+//    @Bean
 //    public WebSecurityCustomizer webSecurityCustomizer() {
 //        return (web) -> web.ignoring().antMatchers("/ignore1", "/ignore2");
 //    }
 
-
-    //настраиваем аутентификацию
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(personDetailsService);
-    }
-
     @Bean
     public PasswordEncoder getPasswordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 
 }
--- src/main/java/ru/sonyabeldy/FirstSecurityApp/services/RegistrationService.java
+++ src/main/java/ru/sonyabeldy/FirstSecurityApp/services/RegistrationService.java
@@ -2,6 +2,7 @@ package ru.sonyabeldy.FirstSecurityApp.services;
 
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import ru.sonyabeldy.FirstSecurityApp.models.Person;
 import ru.sonyabeldy.FirstSecurityApp.repositories.PeopleRepository;
@@ -9,15 +10,20 @@ import ru.sonyabeldy.FirstSecurityApp.repositories.PeopleRepository;
 @Service
 public class RegistrationService {
 
-    public final PeopleRepository peopleRepository;
+    private final PeopleRepository peopleRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public RegistrationService(PeopleRepository peopleRepository) {
+    public RegistrationService(PeopleRepository peopleRepository, PasswordEncoder passwordEncoder) {
         this.peopleRepository = peopleRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Transactional
     public void register(Person person) {
+        String encodedPassword = passwordEncoder.encode(person.getPassword());
+        person.setPassword(encodedPassword);
+
         peopleRepository.save(person);
     }
 }
