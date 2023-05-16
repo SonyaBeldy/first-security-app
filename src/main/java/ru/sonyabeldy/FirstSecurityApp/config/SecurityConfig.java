b2d9eb495cefe3740b0e67586202972a05145e42
Sonya <sofya.beldy@mail.ru>
2023-05-16 13:11:26 +1000

    preauthorize annotaion

--- src/main/java/ru/sonyabeldy/FirstSecurityApp/config/SecurityConfig.java
+++ src/main/java/ru/sonyabeldy/FirstSecurityApp/config/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -20,6 +22,7 @@ import ru.sonyabeldy.FirstSecurityApp.services.PersonDetailsService;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true) //обычно не используют в контроллере,
 public class SecurityConfig {
     //Конфигурационный класс
 
--- src/main/java/ru/sonyabeldy/FirstSecurityApp/controller/HelloController.java
+++ src/main/java/ru/sonyabeldy/FirstSecurityApp/controller/HelloController.java
@@ -6,10 +6,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.sonyabeldy.FirstSecurityApp.security.PersonDetails;
-import ru.sonyabeldy.FirstSecurityApp.services.PersonDetailsService;
+import ru.sonyabeldy.FirstSecurityApp.services.AdminService;
 
 @Controller
 public class HelloController {
+
+    private final AdminService adminService;
+
+    public HelloController(AdminService adminService) {
+        this.adminService = adminService;
+    }
+
     @RequestMapping("/hello")
     public String sayHello() {
         return "hello";
@@ -26,6 +33,7 @@ public class HelloController {
 
     @GetMapping("/admin")
     public String adminPage() {
+        adminService.doAdminStuff();
         return "admin";
     }
 }
